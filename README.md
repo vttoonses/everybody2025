d386cc2c6230bda2df00745863298d33fdd42c06
Eugene Thompson <gene@thompson93.us>
2025-12-06 20:45:54 -0500

    q01 p3 complete

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # everybody2025
 The November 2025 (yeah, I'm late) Everybody Codes challenges! In Lisp!
+
+(note: I'm stealing the tokenizer function from the adventofcode2025 repo)
--- q01.lisp
+++ q01.lisp
@@ -1,19 +1,31 @@
-(defun move-index (index turns name-count)
+(defun move-index (index turns name-list)
   (if turns
-    (let* ((turn (car turns)) (direction (char turn 0)) (distance (parse-integer (subseq turn 1))))
+    (let* ((turn (car turns)) (direction (char turn 0)) (distance (parse-integer (subseq turn 1))) (name-count (length name-list)))
       (cond
-        ((char= direction #\L) (move-index (decf index (if (<= distance index) distance index)) (cdr turns) name-count))
-        (t (move-index (incf index (if (< (+ distance index) name-count) distance (- name-count 1 index))) (cdr turns) name-count))))
-    index))
+        ((char= direction #\L) (move-index (decf index (if (<= distance index) distance index)) (cdr turns) name-list))
+        (t (move-index (incf index (if (< (+ distance index) name-count) distance (- name-count 1 index))) (cdr turns) name-list))))
+    (nth index name-list)))
 
-(defun move-parent-index (index turns name-count)
+(defun move-parent-index (index turns name-list)
   (if turns
-    (let* ((turn (car turns)) (direction (char turn 0)) (distance (parse-integer (subseq turn 1))))
+    (let* ((turn (car turns)) (direction (char turn 0)) (distance (parse-integer (subseq turn 1))) (name-count (length name-list)))
       (cond
         ((char= direction #\L)
-          (move-parent-index (mod (decf index distance) name-count) (cdr turns) name-count))
-        (t (move-parent-index (mod (incf index distance) name-count) (cdr turns) name-count))))
-    index))
+          (move-parent-index (mod (decf index distance) name-count) (cdr turns) name-list))
+        (t (move-parent-index (mod (incf index distance) name-count) (cdr turns) name-list))))
+    (nth index name-list)))
+
+(defun find-last-parent (index turns name-list)
+  (if turns
+    (let* (
+      (turn (car turns))
+      (direction (char turn 0))
+      (distance (parse-integer (subseq turn 1)))
+      (name-count (length name-list))
+      (target (cond ((char= direction #\L) (mod (- index distance) name-count)) (t (mod (+ index distance) name-count)))))
+        (rotatef (nth 0 name-list) (nth target name-list))
+        (find-last-parent 0 (cdr turns) name-list)))
+    (nth 0 name-list))
 
 (defun find-name (filename move-func)
   (with-open-file (in filename :direction :input)
@@ -22,4 +34,4 @@
         (index 0)
         (name-list (tokenizer (read-line in nil) :delimiterp #'(lambda (char) (char= char #\,))))
         (turn-list (progn (read-line in) (tokenizer (read-line in nil) :delimiterp #'(lambda (char) (char= char #\,))))))
-          (nth (funcall move-func index turn-list (length name-list)) name-list)))))
+          (funcall move-func index turn-list name-list)))))
