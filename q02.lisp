9eebe162a04e724d41ffe494e5daa64294c2d1a3
Eugene Thompson <gene@thompson93.us>
2025-12-07 19:28:11 -0500

    q02 p3 complete

--- q02.lisp
+++ q02.lisp
@@ -2,7 +2,7 @@
   (mapcar #'+ op1 op2))
 
 (defun complex-/ (op1 op2)
-  (mapcar #'floor op1 op2))
+  (mapcar #'truncate op1 op2))
 
 (defun complex-* (op1 op2)
   (list
@@ -17,3 +17,21 @@
       (setf num (complex-+ num a))
       (cycle (1- iterations) num a))
     num))
+
+(defun engrave-point? (a)
+  (let ((res '(0 0)))
+    (loop for lcv from 0 below 100 do
+      (setf res (complex-* res res))
+      (setf res (complex-/ res '(100000 100000)))
+      (setf res (complex-+ res a))
+      (if (or (< (first res) -1000000) (< (second res) -1000000) (> (first res) 1000000) (> (second res) 1000000))
+        (return-from engrave-point? nil)))
+    t))
+
+(defun check-pattern (start-point iterations step-size)
+  (let ((point-count 0) (x (first start-point)) (y (second start-point)))
+    (loop for row from y to (+ y iterations) by step-size do
+      (loop for col from x to (+ x iterations) by step-size do
+        (if (engrave-point? (list col row))
+          (incf point-count))))
+    point-count))
